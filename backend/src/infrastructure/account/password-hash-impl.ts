0071897cb2d3467f094648a6ce1e2d604314ed0b
tamurakeito <kit_tamtam@icloud.com>
2025-07-08 16:22:17 +0900

    password hash 実装

---
+++ backend/src/infrastructure/account/password-hash-impl.ts
@@ -0,0 +1,10 @@
+import type { PasswordHash } from "@/domain/account/password-hash.js";
+import bcrypt from "bcrypt";
+
+export class PasswordHashImpl implements PasswordHash {
+  private readonly saltRounds = 10;
+
+  async hash(password: string): Promise<string> {
+    return await bcrypt.hash(password, this.saltRounds);
+  }
+}
