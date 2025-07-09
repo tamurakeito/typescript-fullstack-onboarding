6b2e5ec397990cfac7a6f28dea1d5cfb08cc28f1
tamurakeito <kit_tamtam@icloud.com>
2025-07-09 16:22:24 +0900

    不要なtry-catch 外す

--- backend/src/presentation/handlers/auth-handler.ts
+++ backend/src/presentation/handlers/auth-handler.ts
@@ -12,23 +12,17 @@ export class AuthHandler {
 
     console.log("signIn: ", { userId, password });
 
-    try {
-      if (!userId || !password) {
-        const error = new BadRequestError();
-        return c.json({ message: error.message }, error.statusCode);
-      }
-
-      const user = await this.authQuery.execute(userId, password);
-
-      if (user.isErr()) {
-        return c.json({ message: user.error.message }, user.error.statusCode);
-      }
-
-      return c.json(user.value, 200);
-    } catch (error) {
-      console.error(error);
-      const err = new UnexpectedError();
-      return c.json({ message: err.message }, err.statusCode);
+    if (!userId || !password) {
+      const error = new BadRequestError();
+      return c.json({ message: error.message }, error.statusCode);
     }
+
+    const user = await this.authQuery.execute(userId, password);
+
+    if (user.isErr()) {
+      return c.json({ message: user.error.message }, user.error.statusCode);
+    }
+
+    return c.json(user.value, 200);
   }
 }
