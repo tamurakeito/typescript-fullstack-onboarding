ba3c42178fca9c1a6b3ea7b983831b84b1952eaf
tamurakeito <kit_tamtam@icloud.com>
2025-07-16 12:00:38 +0900

    dialog form の変更

--- frontend/src/features/organization/dialog/create-dialog.tsx
+++ frontend/src/features/organization/dialog/create-dialog.tsx
@@ -70,7 +70,10 @@ export const OrganizationCreateDialog = ({
   return (
     <Dialog open={isOpenCreateDialog} onOpenChange={() => setIsOpenCreateDialog(false)}>
       <DialogContent>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg"
+        >
           <DialogHeader>
             <DialogTitle>組織を新規作成</DialogTitle>
             <DialogDescription>新しく作成する組織の名称を設定してください。</DialogDescription>
