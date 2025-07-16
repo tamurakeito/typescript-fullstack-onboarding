b5303b4ce6bf1d68bea2aefce6cbe0f2e1e9d207
tamurakeito <kit_tamtam@icloud.com>
2025-07-16 13:49:16 +0900

    dialog form 切り出し

--- frontend/src/features/organization/dialog/create-dialog.tsx
+++ frontend/src/features/organization/dialog/create-dialog.tsx
@@ -5,14 +5,13 @@ import {
 import type { zCreateOrganizationRequest } from "@/client/zod.gen";
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
   DialogClose,
-  DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { DialogForm } from "@/components/ui/dialog-form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -68,32 +67,31 @@ export const OrganizationCreateDialog = ({
   };
 
   return (
-    <Dialog open={isOpenCreateDialog} onOpenChange={() => setIsOpenCreateDialog(false)}>
-      <DialogContent>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg"
-        >
-          <DialogHeader>
-            <DialogTitle>組織を新規作成</DialogTitle>
-            <DialogDescription>新しく作成する組織の名称を設定してください。</DialogDescription>
-          </DialogHeader>
-          <Controller
-            control={control}
-            name="name"
-            render={({ field }) => (
-              <Input type="text" placeholder="組織名を入力してください" {...field} />
-            )}
-          />
-          {errors.name?.message && <p className="text-red-700">{errors.name.message}</p>}
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant={"outline"}>キャンセル</Button>
-            </DialogClose>
-            <Button type="submit">作成する</Button>
-          </DialogFooter>
-        </form>
-      </DialogContent>
-    </Dialog>
+    <DialogForm
+      open={isOpenCreateDialog}
+      onOpenChange={() => setIsOpenCreateDialog(false)}
+      formProps={{
+        onSubmit: handleSubmit(onSubmit),
+      }}
+    >
+      <DialogHeader>
+        <DialogTitle>組織を新規作成</DialogTitle>
+        <DialogDescription>新しく作成する組織の名称を設定してください。</DialogDescription>
+      </DialogHeader>
+      <Controller
+        control={control}
+        name="name"
+        render={({ field }) => (
+          <Input type="text" placeholder="組織名を入力してください" {...field} />
+        )}
+      />
+      {errors.name?.message && <p className="text-red-700">{errors.name.message}</p>}
+      <DialogFooter>
+        <DialogClose asChild>
+          <Button variant={"outline"}>キャンセル</Button>
+        </DialogClose>
+        <Button type="submit">作成する</Button>
+      </DialogFooter>
+    </DialogForm>
   );
 };
--- frontend/src/features/organization/dialog/update-dialog.tsx
+++ frontend/src/features/organization/dialog/update-dialog.tsx
@@ -6,14 +6,13 @@ import {
 import type { zUpdateOrganizationRequest } from "@/client/zod.gen";
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
   DialogClose,
-  DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { DialogForm } from "@/components/ui/dialog-form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -81,32 +80,31 @@ export const OrganizationUpdateDialog = ({
   };
 
   return (
-    <Dialog open={openEditDialog !== undefined} onOpenChange={() => setOpenEditDialog(undefined)}>
-      <DialogContent>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg"
-        >
-          <DialogHeader>
-            <DialogTitle>組織名を編集</DialogTitle>
-            <DialogDescription>組織の名称を変更できます。</DialogDescription>
-          </DialogHeader>
-          <Controller
-            control={control}
-            name="name"
-            render={({ field }) => (
-              <Input type="text" placeholder="組織名を入力してください" {...field} />
-            )}
-          />
-          {errors.name?.message && <p className="text-red-700">{errors.name.message}</p>}
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant={"outline"}>キャンセル</Button>
-            </DialogClose>
-            <Button type="submit">保存する</Button>
-          </DialogFooter>
-        </form>
-      </DialogContent>
-    </Dialog>
+    <DialogForm
+      open={openEditDialog !== undefined}
+      onOpenChange={() => setOpenEditDialog(undefined)}
+      formProps={{
+        onSubmit: handleSubmit(onSubmit),
+      }}
+    >
+      <DialogHeader>
+        <DialogTitle>組織名を編集</DialogTitle>
+        <DialogDescription>組織の名称を変更できます。</DialogDescription>
+      </DialogHeader>
+      <Controller
+        control={control}
+        name="name"
+        render={({ field }) => (
+          <Input type="text" placeholder="組織名を入力してください" {...field} />
+        )}
+      />
+      {errors.name?.message && <p className="text-red-700">{errors.name.message}</p>}
+      <DialogFooter>
+        <DialogClose asChild>
+          <Button variant={"outline"}>キャンセル</Button>
+        </DialogClose>
+        <Button type="submit">保存する</Button>
+      </DialogFooter>
+    </DialogForm>
   );
 };
