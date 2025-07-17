225013233dbf996a4fc9fc1ff15786c219bddd30
tamurakeito <kit_tamtam@icloud.com>
2025-07-17 10:37:31 +0900

    tokenにorganizationIdを付与

--- backend/src/presentation/middleware/jwt.ts
+++ backend/src/presentation/middleware/jwt.ts
@@ -2,16 +2,19 @@ import type { JwtService } from "@/infrastructure/account/jwt-service.js";
 import { createMiddleware } from "hono/factory";
 
 export const jwtMiddleware = (jwtService: JwtService) => {
-  return createMiddleware<{ Variables: { role: string } }>(async (c, next) => {
-    const token = c.req.header("Authorization")?.split(" ")[1];
-    if (!token) {
-      return c.json({ error: "Unauthorized" }, 401);
+  return createMiddleware<{ Variables: { role: string; organizationId: string } }>(
+    async (c, next) => {
+      const token = c.req.header("Authorization")?.split(" ")[1];
+      if (!token) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+      const decoded = await jwtService.verify(token);
+      if (decoded.isErr()) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+      c.set("role", decoded.value.role);
+      c.set("organizationId", decoded.value.organizationId);
+      return next();
     }
-    const decoded = await jwtService.verify(token);
-    if (decoded.isErr()) {
-      return c.json({ error: "Unauthorized" }, 401);
-    }
-    c.set("role", decoded.value.role);
-    return next();
-  });
+  );
 };
