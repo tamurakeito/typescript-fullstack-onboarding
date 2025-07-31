b9f5395d44c445a7fa1c452b79db48e7619a4018
tamurakeito <kit_tamtam@icloud.com>
2025-07-31 13:42:33 +0900

    cloud sql

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,41 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+// VPCネットワーク作成
+resource "google_compute_network" "peering_network" {
+  name                    = "private-network"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.peering_network.id
+}
+
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.peering_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+/* Cloud SQL */
+resource "google_sql_database_instance" "default" {
+  name             = "private-ip-sql-instance"
+  database_version = "POSTGRES_14"
+  depends_on = [google_service_networking_connection.default]
+  settings {
+    tier = "db-custom-2-7680"
+    ip_configuration {
+      ipv4_enabled    = "false" // パブリックIPを無効化
+      private_network = google_compute_network.peering_network.id // プライベートIPを持たせる
+    }
+  }
+}
+
+/* Cloud Storage */
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
